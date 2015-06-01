f63db8060d75da86a40fbad27ebec1f015829497
Teddy <Teddy@mail.com>
2015-06-01 16:03:30 +1000

    added new unittests httpBasedService

--- test/e2e/richlist_test.js
+++ test/e2e/richlist_test.js
@@ -1,23 +1,17 @@
-describe('celebrity e2e tests', function() {
-  it('should display 1 items', function() {
+describe('celebrity e2e tests', function () {
+  it('should display 3 ozzies', function () {
     browser.get('http://localhost:9000/#/');
-
     element(by.model('birthPlace')).sendKeys('Australia');
-   // element(by.css('[value="add"]')).click();
-
-
-   var table= element(by.css(".table-striped"));
-
-
-
-    var persons= element.all(by.repeater('person in list'));
-    //debugger;
+    var table = element(by.css(".table-striped"));
+    var persons = element.all(by.repeater('person in list'));
     expect(persons.count()).toEqual(3);
+  });
 
-    //
-    //// You wrote your first test, cross it off the list
-    //todoList.get(2).element(by.css('input')).click();
-    //var completedAmount = element.all(by.css('.done-true'));
-    //expect(completedAmount.count()).toEqual(2);
+  it('should display 2 dutchies', function () {
+    browser.get('http://localhost:9000/#/');
+    element(by.model('birthPlace')).sendKeys('Netherlands');
+    var table = element(by.css(".table-striped"));
+    var persons = element.all(by.repeater('person in list'));
+    expect(persons.count()).toEqual(2);
   });
 });
--- test/spec/controllers/about.js
+++ test/spec/controllers/about.js
@@ -3,7 +3,7 @@
 describe('Controller: AboutCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('angularmainForkApp'));
+  beforeEach(module('app'));
 
   var AboutCtrl,
     scope;
